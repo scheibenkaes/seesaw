777301fee7a6edac5c5099797376b05cade981da
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-06-07 21:31:18 +0200

    Added hints to seesaw.color

--- src/seesaw/color.clj
+++ src/seesaw/color.clj
@@ -12,13 +12,13 @@
   (:import [java.awt Color]))
 
 (defn get-rgba 
-  [c] 
+  [^Color c] 
   (vector (.getRed c) (.getGreen c) (.getBlue c) (.getAlpha c)))
 
 (defn color
   ([s] (Color/decode s))
   ([s a] (apply color (assoc (get-rgba (color s)) 3 a)))
-  ([r g b a] (Color. r (or g 0) (or b 0) (or a 255)))
+  ([r g b a] (Color. ^Integer r (or ^Integer g 0) (or ^Integer b 0) (or ^Integer a 255)))
   ([r g b] (color r g b nil)))
 
 (defn default-color
